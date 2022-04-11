6dad51c633f516af73f1833f4d67897f18bf1219
vladimirlarass <84715178+vladimirlarass@users.noreply.github.com>
2022-04-11 17:18:49 +0100

    todo_list

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+// DOM selection
+const list = document.getElementById('list')
+const input = document.getElementById('input')
+const add = document.getElementById('add')
+const clear = document.getElementById('clear')
+const url = document.getElementById('url')
+const load = document.getElementById('load')
+
+// on recupere les taches 
+
+const tasks = 'une chaine, de caractère'
+
+load.style.background = 'red'
